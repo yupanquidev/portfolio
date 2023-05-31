78e612fed1a377c28f8ff959bfe91951683dfa88
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-05-31 18:54:57 -0500

    fix: deploy

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
-FROM pierrezemb/gostatic
 FROM node:18-alpine AS base
 
 RUN npm i -g pnpm
-
-FROM base AS dependecies
 
 WORKDIR /app
 COPY package.json .
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8080
+  },
   integrations: [tailwind()]
 });
