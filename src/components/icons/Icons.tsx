486701665effc6796d5c92d0cce8a25aaf29fdb0
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-08-13 10:17:34 -0500

    fix: spacing

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -13,10 +13,7 @@ export const IconCode = () => (
     <path d='M7 8l-4 4l4 4' />
     <path d='M17 8l4 4l-4 4' />
     <path d='M14 4l-4 16' />
-  </svg>
-
-
-  
+  </svg>  
 )
 
 export const IconBrandThreads = () => {
