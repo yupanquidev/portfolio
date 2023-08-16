d1c136e180b7290e1a2ef38d5384ac59434afe3d
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-08-15 23:14:27 -0500

    fix: icon download and add styles

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -1,5 +1,5 @@
 export const IconDownload = () => (
-  <svg xmlns='http://www.w3.org/2000/svg' className='group-hover:rotate-12 transition-all duration-500 stroke-zinc-100' width='22' height='20' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+  <svg xmlns='http://www.w3.org/2000/svg' className='group-hover:rotate-12 transition-all duration-500 stroke-zinc-100' width='24' height='22' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
     <path stroke='none' d='M0 0h24v24H0z' fill='none' />
     <path d='M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2' />
     <path d='M7 11l5 5l5 -5' />
