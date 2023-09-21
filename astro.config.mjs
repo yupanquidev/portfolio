afeec7c3474caa30127b8137de148255ac30908e
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-09-21 08:49:30 -0500

    delete: dockerfile and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,5 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: true,
-    strictPort: true,
-    port: 8080
-  },
   integrations: [tailwind(), react()]
 })
