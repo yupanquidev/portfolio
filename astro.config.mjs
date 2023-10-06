3841b48eebc39ff6dbf26f5f95982fe022ed59cd
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-10-06 11:20:30 -0500

    delete: react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind()]
 })
