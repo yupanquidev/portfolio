75fa5a63b0843c3e54fe8053ef255120b68c6577
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-11-15 10:57:14 -0500

    Add Xcard interface and use it in Xcard component

---
+++ src/lib/data.ts
@@ -0,0 +1,19 @@
+export interface Xcard {
+  avatar: string;
+  fullName: string;
+  username: string;
+  xUrl: string;
+  tweet: string;
+  following: number;
+  followers: number;
+}
+
+export const Xcard = {
+  avatar: "https://github.com/yupanquidev.png",
+  fullName: "Humberto Yupanqui Apaza",
+  username: "@yupanquidev",
+  xUrl: "https://twitter.com/yupanquidev",
+  tweet: "Hola, soy nuevo en Twitter y no me gustan mucho las redes sociales 🙃 Estoy buscando conectarme con otros desarrolladores y aprender sobre lo último en desarrollo web 🤗",
+  following: 40,
+  followers: 1,
+}
