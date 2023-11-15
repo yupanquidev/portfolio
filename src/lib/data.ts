9e51f46255a2d6012e82c58c2dafea134f4c515a
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-11-15 17:25:23 -0500

    Update xcard data in data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -12,8 +12,8 @@ export const xcard: Xcard = {
   avatar: "https://github.com/yupanquidev.png",
   fullName: "Humberto Yupanqui Apaza",
   username: "@yupanquidev",
-  xUrl: "https://twitter.com/yupanquidev",
-  tweet: "Hola, soy nuevo en Twitter y no me gustan mucho las redes sociales 🙃 Estoy buscando conectarme con otros desarrolladores y aprender sobre lo último en desarrollo web 🤗",
-  following: 40,
+  xUrl: "https://x.com/yupanquidev",
+  tweet: "Hola, soy nuevo en X y no me gustan mucho las redes sociales 🙃 Estoy buscando conectarme con otros desarrolladores y aprender sobre lo último en desarrollo web 🤗",
+  following: 46,
   followers: 1,
 };
