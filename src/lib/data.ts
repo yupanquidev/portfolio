29cf6319c0dbb79de11e74f2807c29478df137f2
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-12-05 10:38:27 -0500

    Refactor technology tags with color styles

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -17,3 +17,31 @@ export const xcard: Xcard = {
   following: 40,
   followers: 1,
 };
+
+export const colors = {
+  color: {
+    javascript: "#f0db4f",
+    typescript: "#007acc",
+    php: "#777bb3",
+    html: "#e34c26",
+    css: "#264de4",
+    vue: "#41b883",
+    react: "#61dbfb",
+    angular: "#dd0031",
+    node: "#026e00",
+    laravel: "#f55247",
+    mysql: "#4479a1",
+    postgresql: "#336791",
+    mongodb: "#13aa52",
+    oracledatabase: "#f80000",
+    sqlserver: "#cc2927",
+    redis: "#dc382d",
+    docker: "#2496ed",
+    taiwindcss: "#06b6d4",
+    python: "#3776ab",
+    java: "#007396",
+    patronesdediseño: "#fff",
+    patronesarquitectura: "#fff",
+    ia: "#fff",
+  }
+};
