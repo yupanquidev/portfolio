dae7c775a5241e819ebfd9206fced12ce5a3eaf8
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-12-06 09:09:44 -0500

    Refactor import statements and update tag colors

---
+++ src/lib/xdata.ts
@@ -0,0 +1,19 @@
+interface Xcard {
+  avatar: string;
+  fullName: string;
+  username: string;
+  xUrl: string;
+  tweet: string;
+  following: number;
+  followers: number;
+}
+
+export const xcard: Xcard = {
+  avatar: "https://github.com/yupanquidev.png",
+  fullName: "Humberto Yupanqui Apaza",
+  username: "@yupanquidev",
+  xUrl: "https:x.com/yupanquidev",
+  tweet: "Hola, soy nuevo en X y no me gustan mucho las redes sociales 🙃 Estoy buscando conectarme con otros desarrolladores y aprender sobre lo último en desarrollo web 🤗",
+  following: 40,
+  followers: 1,
+};
