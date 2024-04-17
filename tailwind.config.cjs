d0fb15f267267b93c6da331499632f23b7f50312
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-04-16 21:37:43 -0500

    fix: config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}'],
   theme: {
     fontFamily: {},
-    extend: {}
+    extend: {
+      boxShadow: {
+        'lg': '0px 0px 15px -1px rgb(0 0 0 / 0.1)'
+      }
+    }
   },
   plugins: []
 }
