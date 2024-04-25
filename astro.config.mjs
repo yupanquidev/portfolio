ceacff724f761e92a94a99b43d45af47317f1bc1
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-04-24 21:45:25 -0500

    add: i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig, passthroughImageService } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService(),
-  },
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
+})
