11c5e9d4507495d8238619d444d5ea7f7692b3c4
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-08-08 11:23:26 -0500

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "hybrid",
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
