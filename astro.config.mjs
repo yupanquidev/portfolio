4d05fb106b5985ec538d5f73361b00b20655f707
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-09-20 11:00:10 -0500

    remove output hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
+  output: "server",
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
