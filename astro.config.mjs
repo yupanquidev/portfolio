fadb51fe14f90ac377c38f162868dfbbb11066e8
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-12-05 10:23:56 -0500

    fix import astro/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig, envField } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercelAdapter from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelAdapter(),
   prefetch: true,
-
   env: {
     schema: {
       GEOLOCATION: envField.string({ context: "client", access: "public" }),
